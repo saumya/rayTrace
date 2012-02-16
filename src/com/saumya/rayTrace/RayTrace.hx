650966551be2218fd297ac729846f59450087cd6
saumya <2saumyaray@gmail.com>
2012-02-17 01:47:38 +0530

    added components

--- src/com/saumya/rayTrace/RayTrace.hx
+++ src/com/saumya/rayTrace/RayTrace.hx
@@ -1,12 +1,13 @@
 package com.saumya.rayTrace;
 //import haxe.remoting.LocalConnection;
+import com.saumya.raytrace.components.ButtonBase;
 import flash.net.LocalConnection;
 
 import nme.display.Sprite;
 import nme.text.TextField;
 
 /**
- * ...
+ * @version 1.0.0
  * @author saumya
  */
 
@@ -26,6 +27,24 @@ class RayTrace extends Sprite
 	}
 	public function init():Void
 	{
+		var upBtn:ButtonBase = new ButtonBase();
+		upBtn.setLabel('up');
+		upBtn.setWidth(30);
+		upBtn.x = 405;
+		this.addChild(upBtn);
+		var dnBtn:ButtonBase = new ButtonBase();
+		dnBtn.setLabel('dn');
+		dnBtn.setWidth(30);
+		dnBtn.x = 405;
+		dnBtn.y = 380;
+		this.addChild(dnBtn);
+		var clearBtn:ButtonBase = new ButtonBase();
+		clearBtn.setLabel('clear');
+		clearBtn.setWidth(50);
+		clearBtn.x = 10;
+		clearBtn.y = 400;
+		this.addChild(clearBtn);
+		//
 		this.logNumber = 0;
 		this.logMessage = '';
 		//
@@ -35,7 +54,10 @@ class RayTrace extends Sprite
 		this.t.selectable = true;
 		this.t.width = 400;
 		this.t.height = 400;
-		this.t.text = 'RayTrace V 1.0.0';
+		this.t.text = 'RayTrace V 2.0.0';
+		this.t.background = true;
+		this.t.backgroundColor = 0xEEEEEE;
+		
 		this.addChild(this.t);
 		//connect
 		this.lc = new LocalConnection();
---
+++ src/com/saumya/rayTrace/components/ButtonBase.hx
@@ -0,0 +1,43 @@
+package com.saumya.raytrace.components;
+import nme.display.Sprite;
+import nme.display.Graphics;
+import nme.text.TextField;
+
+class ButtonBase extends Sprite
+{
+	private var t:TextField;
+	public function new():Void
+	{
+		super();
+		this.init();
+	}
+	private function init():Void
+	{
+		this.drawBG();
+		this.t = new TextField();
+		this.t.width = 50;
+		this.addChild(this.t);
+	}
+	
+	private function drawBG():Void
+	{
+		var g:Graphics = this.graphics;
+		g.clear();
+		g.beginFill(0xAAAAAA);
+		g.drawRect(0,0,20,20);
+	}
+	
+	public function setLabel(s:String):Void
+	{
+		this.t.text = s;
+	}
+	public function setWidth(width:Float):Void
+	{
+		this.width = width;
+		//
+		var g:Graphics = this.graphics;
+		g.clear();
+		g.beginFill(0xAAAAAA);
+		g.drawRect(0,0,width,20);
+	}
+}
