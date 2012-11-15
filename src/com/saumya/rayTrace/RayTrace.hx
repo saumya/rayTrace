5da77ab993d8562e422f9358d8b4503d6ff49596
saumya <2saumyaray@gmail.com>
2012-11-15 16:08:18 +0530

    scrollbar is added and functional. TODO, fix the UX of scroll

--- src/com/saumya/rayTrace/RayTrace.hx
+++ src/com/saumya/rayTrace/RayTrace.hx
@@ -3,6 +3,7 @@ package com.saumya.rayTrace;
 import com.saumya.raytrace.components.ButtonBase;
 import flash.events.Event;
 import flash.events.StatusEvent;
+import flash.errors.ArgumentError;
 import flash.net.LocalConnection;
 
 import flash.display.Sprite;
@@ -13,7 +14,8 @@ import minimalcomps.PushButton;
 import minimalcomps.Label;
 import minimalcomps.Text;
 import minimalcomps.InputText;
-import flash.errors.ArgumentError;
+import minimalcomps.Panel;
+import minimalcomps.VSlider;
 
 /**
  * @version 4.1.0
@@ -32,10 +34,10 @@ class RayTrace extends Sprite
 	private var isInAdvancedMode:Bool;
 	private var channelInfo:Label;
 	//
-	private var scrollTrack:PushButton;
 	private var shouldScroll:Bool;
 	//
-	private var lastPosY:Float;
+	private var p:Panel;
+	private var vs:VSlider;
 	
 	public function new() 
 	{
@@ -44,38 +46,30 @@ class RayTrace extends Sprite
 	}
 	public function init():Void
 	{
-		var upBtn:PushButton=new PushButton(this,605,0,'up');
-		upBtn.setSize(30,20);
-		var dnBtn:PushButton=new PushButton(this,605,380,'dn');
-		dnBtn.setSize(30,20);
+		this.p=new Panel(this, 0, 0);
+		p.setSize(640,400);
+		p.setColor(0xAAAAAA);
+		this.vs=new VSlider(this.p, 630, 0,onUserSlide);
+		this.vs.setSize(15,400);
+		this.vs.setSliderParams(0,590,590);
 		var clearBtn:PushButton=new PushButton(this,0,410,'clear');
 		clearBtn.setSize(50,20);
 		//chanel info
 		this.channelInfo=new Label(this,70,415,'Connection name : _RayTrace_V4.1.0_');
 		//
 		clearBtn.addEventListener(MouseEvent.CLICK, onClear);
-		dnBtn.addEventListener(MouseEvent.CLICK, onDown);
-		upBtn.addEventListener(MouseEvent.CLICK, onUp);
 		//test button
 		var testBtn:PushButton=new PushButton(this,645,0,'test');
 		testBtn.setSize(40,20);
-		testBtn.visible=false;
+		testBtn.visible=false;//test button
 		testBtn.addEventListener(MouseEvent.CLICK, onTest);
-		//
-		this.scrollTrack=new PushButton(this,605,25,'');
-		this.scrollTrack.setSize(20,20);
-		this.scrollTrack.addEventListener(MouseEvent.MOUSE_DOWN, onScrollMouseDown);
-		this.scrollTrack.addEventListener(MouseEvent.MOUSE_UP, onScrollMouseUp);
-		this.scrollTrack.addEventListener(MouseEvent.MOUSE_MOVE, onScrollMouseMove);
-		this.shouldScroll = false;
-		this.scrollTrack.visible=false;
-		upBtn.visible=dnBtn.visible=false;
 		//
 		this.logNumber = 0;
 		this.logMessage = '';
 		//
-		this.t=new Text(this,0,0,'Hello Text');
-		this.t.setSize(600,400);
+		this.t=new Text(this,10,10,'Hello Text');
+		//this.t=new Text(p,50,50,'Hello Text');
+		this.t.setSize(610,380);
 		this.t.text='RayTrace V4.1.0';
 		//
 		var btnStart:PushButton=new PushButton(this,555,415,'Start');
@@ -151,74 +145,10 @@ class RayTrace extends Sprite
 		}
 	}
 	
-	private function onScrollMouseMove(e:MouseEvent):Void 
-	{
-		var currentPosY:Float = e.stageY;
-		//check for the scroll direction
-		if (this.shouldScroll)
-		{
-			/*
-			//this.log('onScrollMouseMove');
-			//this.log('currentPosY=' + currentPosY + ' :: lastPos=' + this.lastPosY);
-			if (currentPosY>this.lastPosY)
-			{
-				//moves the text up
-				this.t.scrollV++;
-			}else {
-				//moves the text down
-				this.t.scrollV--;
-			}
-			
-			//update the last position
-			this.lastPosY = currentPosY;
-			*/
-			/*
-			var ms:Int = this.t.maxScrollV;
-			var scrollHeight = (350 - 30); // min pos=30, max pos=360
-			var percentScroll:Int = Math.ceil((ms / scrollHeight) * (this.scrollTrack.y));
-			this.t.scrollV = percentScroll;
-			//
-			this.scrollTrack.y = this.mouseY - (this.scrollTrack.height / 2);
-			//restricts the scroll track
-			if (this.scrollTrack.y<=30)
-			{
-				this.scrollTrack.y = 30;
-			}else if(this.scrollTrack.y>=350) {
-				this.scrollTrack.y = 350;
-			}
-			*/
-		}
-		
-	}
-	
-	private function onScrollMouseUp(e:MouseEvent):Void 
-	{
-		//this.log('onScrollMouseUp');
-		this.shouldScroll = false;
-		//
-		this.lastPosY = this.mouseY;
-	}
-	
-	private function onScrollMouseDown(e:MouseEvent):Void 
-	{
-		//this.log('onScrollMouseDown');
-		this.shouldScroll = true;
-	}
-	
 	private function onTest(e:MouseEvent):Void 
 	{
 		//this.log('hello : max scroll='+this.t.maxScrollV);
 		this.log('onTest : checking');
-	}
-	
-	private function onUp(e:MouseEvent):Void 
-	{
-		//this.t.scrollV--;
-	}
-	
-	private function onDown(e:MouseEvent):Void 
-	{
-		//this.t.scrollV++;
 	}
 	
 	private function onClear(e:MouseEvent):Void 
@@ -227,6 +157,15 @@ class RayTrace extends Sprite
 		this.logMessage = 'RayTrace V4.1.0';
 		this.t.text = this.logMessage;
 	}
+	private function onUserSlide(e:Event):Void{
+		//this.log(this.vs.getValue()+'');
+		var tf:TextField=this.t.getTextField();
+		var min:Int=0;
+		var max:Int=tf.maxScrollV;
+		var percent:Float=(max/400)*(this.vs.getValue());
+		var n:Int=Math.round(percent);
+		tf.scrollV=n;
+	}
 	
 	public function log(message:String):Void
 	{
@@ -234,6 +173,11 @@ class RayTrace extends Sprite
 		this.logNumber++;
 		//this.logMessage = message + '\n' + this.logNumber + ' : ' + s;
 		this.t.text = this.logNumber+' : '+message + '\n' + s;
+		/*
+		var tf:TextField=this.t.getTextField();
+		this.vs.setValue(tf.maxScrollV);
+		 */
+		 //TODO: Fix the scroll position
 	}
 	
 	
